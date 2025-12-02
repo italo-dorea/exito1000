7fa92ce657ace6b698a1b09a052e07349c22fcf5
ifdc2308 <italodorea123@gmail.com>
2025-12-01 22:57:48 -0300

    aos-fade

--- script.js
+++ script.js
@@ -18,3 +18,16 @@ document.querySelectorAll("#nav-mobile a").forEach(link => {
     navMobile.classList.remove("active");
   });
 });
+
+
+//effect FADE elements
+document.addEventListener('DOMContentLoaded', function () {
+    AOS.init({
+      duration: 1200,      // duração da animação (ms)
+      easing: 'ease-out', // suavização
+      once: false,         // anima só na primeira vez
+      offset: 80,         // dispara um pouco antes do elemento entrar
+      mirror: true       // não reanima ao subir a página
+    });
+  });
+
